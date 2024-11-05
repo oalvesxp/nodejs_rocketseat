d6592a219effd3b450b9818465a536661ef426ed
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-05 08:35:14 -0300

    Estrutura da aplicação - Salvando usuários em memória (Headers)

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -1,37 +1,27 @@
 import http from 'node:http'
 
-/**
- * Criar usuários
- * Listagem de usuários
- * Edição de usuários
- * Remoção de usuários
- *
- * HTTP
- *  - Method HTTP
- *  - URL
- *
- * METHODS
- *  - GET     -> Buscar um recurso no back-end
- *  - POST    -> Criar um recurso no back-end
- *  - PUT     -> Atualizar um recurso (uma entidade quase que por completo) no back-end
- *  - PATCH   -> Atualiar única e específica de um recurso no back-end
- *  - DELETE  -> Apagar um recurso no back-end
- */
+const users = []
 
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuarios')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Jhon Doe',
+      email: 'jhondoe@example.com',
+    })
     return res.end('Criacao de usuarios')
   }
 
   /** Early return */
-  return res.end('Hello Ignite')
+  return res.end('Hello World!')
 })
 
-/** curl localhost:3333 */
 server.listen(3333)
