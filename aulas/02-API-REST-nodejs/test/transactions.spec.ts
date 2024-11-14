ef6b3a99a203e1452d2814a378cc105de6a40594
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-14 11:09:04 -0300

    Testes automatizados - Finalizando testes da aplicação

--- aulas/02-API-REST-nodejs/test/transactions.spec.ts
+++ aulas/02-API-REST-nodejs/test/transactions.spec.ts
@@ -53,4 +53,67 @@ describe('Transactions routes', () => {
       }),
     ])
   })
+
+  it('Should be able to list a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 100,
+        type: 'credit',
+      })
+
+    const cookies: string[] | undefined =
+      createTransactionResponse.get('Set-Cookie')
+
+    const lisTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies ?? [])
+      .expect(200)
+
+    const transactionId = lisTransactionResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies ?? [])
+      .expect(200)
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 100,
+      })
+    )
+  })
+
+  it('Should be able to get the sumary', async () => {
+    const createCreditTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 100,
+        type: 'credit',
+      })
+
+    const cookies: string[] | undefined =
+      createCreditTransactionResponse.get('Set-Cookie')
+
+    const createDebitTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies ?? [])
+      .send({
+        title: 'New transaction',
+        amount: 50,
+        type: 'debit',
+      })
+
+    const getSumaryResponse = await request(app.server)
+      .get(`/transactions/sumary`)
+      .set('Cookie', cookies ?? [])
+      .expect(200)
+
+    expect(getSumaryResponse.body.sumary).toEqual({
+      amount: 50,
+    })
+  })
 })
