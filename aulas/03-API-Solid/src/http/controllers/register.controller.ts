ed9d3fad03a79b055bc3dc7ab8f4db19ca2c7575
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-21 15:30:41 -0300

    Caso de Uso e Design Patterns - Lidando com erros do use case

--- aulas/03-API-Solid/src/http/controllers/register.controller.ts
+++ aulas/03-API-Solid/src/http/controllers/register.controller.ts
@@ -1,4 +1,5 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistisError } from '@/use-cases/errors/user-already-exists-error'
 import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
@@ -22,7 +23,11 @@ export async function register(req: FastifyRequest, rep: FastifyReply) {
       password,
     })
   } catch (err) {
-    return rep.status(409).send()
+    if (err instanceof UserAlreadyExistisError) {
+      return rep.status(409).send({ message: err.message })
+    }
+
+    return rep.status(500).send() // TODO: fix me
   }
 
   return rep.status(201).send()
