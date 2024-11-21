d971743face82e8230657675285fe1bdb5449ed9
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-21 13:25:03 -0300

    Caso de Uso e Design Patterns - Hash da senha e validação

--- aulas/03-API-Solid/src/http/controllers/register.controller.ts
+++ aulas/03-API-Solid/src/http/controllers/register.controller.ts
@@ -1,4 +1,5 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
 
@@ -11,11 +12,21 @@ export async function register(req: FastifyRequest, rep: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(req.body)
 
+  const passwd_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) return rep.status(409).send()
+
   await prisma.user.create({
     data: {
       name,
       email,
-      passwd_hash: password,
+      passwd_hash,
     },
   })
 
