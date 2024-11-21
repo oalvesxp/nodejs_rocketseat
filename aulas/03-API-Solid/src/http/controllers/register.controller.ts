64d5631b46c1713f1716fe5889874cf5b57f899c
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-21 16:01:43 -0300

    Caso de Uso e Design Patterns - Handler de erros global

--- aulas/03-API-Solid/src/http/controllers/register.controller.ts
+++ aulas/03-API-Solid/src/http/controllers/register.controller.ts
@@ -27,7 +27,7 @@ export async function register(req: FastifyRequest, rep: FastifyReply) {
       return rep.status(409).send({ message: err.message })
     }
 
-    return rep.status(500).send() // TODO: fix me
+    throw err
   }
 
   return rep.status(201).send()
