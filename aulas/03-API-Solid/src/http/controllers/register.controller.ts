9b71e07d2772df766dd428664c869267228cb7a1
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-21 13:42:25 -0300

    Caso de Uso e Design Patterns - Caso de uso de registro

--- aulas/03-API-Solid/src/http/controllers/register.controller.ts
+++ aulas/03-API-Solid/src/http/controllers/register.controller.ts
@@ -1,6 +1,5 @@
+import { registerUseCase } from '@/use-cases/register'
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { hash } from 'bcryptjs'
-import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
 
 export async function register(req: FastifyRequest, rep: FastifyReply) {
@@ -12,23 +11,15 @@ export async function register(req: FastifyRequest, rep: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(req.body)
 
-  const passwd_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) return rep.status(409).send()
-
-  await prisma.user.create({
-    data: {
+  try {
+    await registerUseCase({
       name,
       email,
-      passwd_hash,
-    },
-  })
+      password,
+    })
+  } catch (err) {
+    return rep.status(409).send()
+  }
 
   return rep.status(201).send()
 }
