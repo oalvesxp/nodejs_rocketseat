739392d7dd917db6fd441b3e27e63de8caa12539
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-24 13:56:40 -0300

    Implementando casos de uso - Caso de uso de histórico

--- aulas/03-API-Solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ aulas/03-API-Solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -6,6 +6,12 @@ import dayjs from 'dayjs'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
 
+  async findManyByUserId(userId: string, page: number): Promise<CheckIn> {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+  }
+
   async findByUserIdOnDate(
     userId: string,
     date: Date,
