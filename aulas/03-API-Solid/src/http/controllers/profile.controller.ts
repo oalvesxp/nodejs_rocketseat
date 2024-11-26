777f22fd7d5049a97074e350d4ed059b02d02e33
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-26 08:55:55 -0300

    Controllers & Testes E2E - Controller de perfil

--- aulas/03-API-Solid/src/http/controllers/profile.controller.ts
+++ aulas/03-API-Solid/src/http/controllers/profile.controller.ts
@@ -1,9 +1,16 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(req: FastifyRequest, rep: FastifyReply) {
-  await req.jwtVerify()
+  const getUserProfile = makeGetUserProfileUseCase()
+  const { user } = await getUserProfile.execute({
+    userId: req.user.sub,
+  })
 
-  console.log(req.user.sub)
-
-  return rep.status(200).send()
+  return rep.status(200).send({
+    user: {
+      ...user,
+      passwd_hash: undefined,
+    },
+  })
 }
