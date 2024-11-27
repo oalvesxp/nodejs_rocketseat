849bea518213f562fe0bcc8d26b9329d214ec9a6
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-27 09:47:07 -0300

    Controllers & Testes E2E - Teste E2E da autenticação

--- aulas/03-API-Solid/src/http/controllers/authenticate.controller.spec.ts
+++ aulas/03-API-Solid/src/http/controllers/authenticate.controller.spec.ts
@@ -1,3 +1,29 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('[e2e] Authenticate Controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('Shoud be able to authenticated', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Jhon Doe',
+      email: 'jhon.doe@example.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'jhon.doe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({ token: expect.any(String) })
+  })
+})
