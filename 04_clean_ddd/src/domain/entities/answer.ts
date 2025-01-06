df4907f45a94c45a5c4865d46369560af0ae668c
Osvaldo Alves <oalves.neto@outlook.com>
2025-01-06 13:23:08 -0300

    Fundamentos de DDD - Mapeando relacionamentos

--- 04_clean_ddd/src/domain/entities/answer.ts
+++ 04_clean_ddd/src/domain/entities/answer.ts
@@ -1,11 +1,21 @@
 import { randomUUID } from 'node:crypto'
 
-export class Answer {
-  id: string
+interface AnswerProps {
   content: string
+  authorId: string
+  questionId: string
+}
 
-  constructor(content: string, id?: string) {
-    this.content = content
+export class Answer {
+  public id: string
+  public content: string
+  public authorId: string
+  public questionId: string
+
+  constructor(props: AnswerProps, id?: string) {
+    this.content = props.content
+    this.authorId = props.authorId
+    this.questionId = props.questionId
     this.id = id ?? randomUUID()
   }
 }
