ae13c698f155721627f76ab999f86a4b1c3b7064
Osvaldo Alves <oalves.neto@outlook.com>
2025-01-13 21:12:35 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Criar pergunta

---
+++ 04_clean_ddd/src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,19 @@
+import { Question } from '../../enterprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { CreateQuestionUseCase } from './create-question'
+
+const fakeQuestionsRepository: QuestionsRepository = {
+  create: async (question: Question) => {},
+}
+
+test('Create a question', async () => {
+  const sut = new CreateQuestionUseCase(fakeQuestionsRepository)
+
+  const { question } = await sut.execute({
+    authorId: '1',
+    title: 'Nova pergunta',
+    content: 'Conteúdo da pergunta',
+  })
+
+  expect(question.id).toBeTruthy()
+})
