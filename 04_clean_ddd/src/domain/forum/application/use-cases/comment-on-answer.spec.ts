5cdedbe91075635140b7029acf5f41037ba0f35f
Osvaldo Alves <oalves.neto@outlook.com>
2025-01-29 18:18:24 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Comentar em uma resposta

---
+++ 04_clean_ddd/src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,32 @@
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+import { makeAnswer } from 'test/factories/make-answer'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment on answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsRepository,
+    )
+  })
+
+  it('Should be able to comment on a specific answer', async () => {
+    const answer = makeAnswer()
+    await inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      authorId: 'author01',
+      answerId: answer.id.toString(),
+      content: 'Lorem inpsur...',
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items).toHaveLength(1)
+  })
+})
